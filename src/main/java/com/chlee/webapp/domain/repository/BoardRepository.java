9ba92ce7b17fc01676c694432dd554df6bf6c623
CheolheeLee0 <wks0968@gmail.com>
2021-04-18 17:02:22 +0900

    Pagination 구현 작동 확인

--- src/main/java/com/chlee/webapp/domain/repository/BoardRepository.java
+++ src/main/java/com/chlee/webapp/domain/repository/BoardRepository.java
@@ -3,5 +3,8 @@ package com.chlee.webapp.domain.repository;
 import com.chlee.webapp.domain.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    List<BoardEntity> findByTitleContaining(String keyword);
 }
